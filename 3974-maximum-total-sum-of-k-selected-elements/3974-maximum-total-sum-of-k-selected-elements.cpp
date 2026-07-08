4df8d01b010b5e3147c75c19b99b6a387b412938
Sabasana Aryan <aryan.sabasana.cg@gmail.com>
2026-07-08 19:19:05 +0530

    Time: 186 ms (8.93%), Space: 188.1 MB (20.24%) - LeetHub

--- 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.cpp
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     long long maxSum(vector<int>& nums, int k, int mul) {
-         sort(nums.rbegin() , nums.rend()) ; 
-        long long int sum = 0 ; 
-        for(int i = 0 ; i < k ; i ++){
-            if(mul<1){
-                //  (1LL) *   (*)kae ke likhana hota he 
-                sum += (1LL) * nums[i] ; 
-                mul -- ;
+        sort(nums.rbegin(), nums.rend());
+        long long int sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (mul < 1) {
+                sum += (1LL) * nums[i];
+                mul--;
             }
-            else{
-                // long long direct likha sakte he 
-                sum += (long long) nums[i]*mul ; 
-                mul-- ; 
+
+                else {
+                sum += (long long)nums[i] * mul;
+                mul--;
             }
         }
-        return sum ; 
+
+        return sum;
     }
 };
