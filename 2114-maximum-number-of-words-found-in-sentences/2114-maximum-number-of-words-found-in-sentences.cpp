0bbba7c97b019f943637521db8bf5c10d9a6c096
Sabasana Aryan <aryan.sabasana.cg@gmail.com>
2026-07-12 16:20:47 +0530

    Time: 0 ms (100%), Space: 13.2 MB (50.79%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
-        int len =0;
-        int max = 0;
-        string str = "";
-        for(int i=0; i<sentences.size(); i++){
+       int len = 0;
+       int max = 0;
+       string str = "";
+       for(int i = 0; i<sentences.size(); i++) {
+
             str += sentences[i];
             for(int j=0; j<str.length(); j++) {
                 if(str[j]==' ') {
-                    len++;
+                    len++;  
                 }
             }
             str = "";
-            if(len >= max) {
+            if(len>=max) {
                 max = len;
             }
 
-            len =0;
-        }
-        return max + 1;
+            len = 0;
+
+       }
+       return max + 1;
     }
 };
