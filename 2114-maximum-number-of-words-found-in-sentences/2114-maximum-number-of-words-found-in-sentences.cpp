1d93463e9591f90b88a3b5b8dd9405cdfa3e09fe
Sabasana Aryan <aryan.sabasana.cg@gmail.com>
2026-07-12 10:28:48 +0530

    Time: 0 ms (100%), Space: 13.5 MB (31.9%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int len =0;
+        int max = 0;
+        string str = "";
+        for(int i=0; i<sentences.size(); i++){
+            str += sentences[i];
+            for(int j=0; j<str.length(); j++) {
+                if(str[j]==' ') {
+                    len++;
+                }
+            }
+            str = "";
+            if(len >= max) {
+                max = len;
+            }
+
+            len =0;
+        }
+        return max + 1;
+    }
+};
