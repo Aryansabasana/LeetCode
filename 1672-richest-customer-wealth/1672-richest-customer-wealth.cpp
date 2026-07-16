70583b264d361ca54ddca5fdd1ee5576f8ac7116
Sabasana Aryan <aryan.sabasana.cg@gmail.com>
2026-07-16 21:41:27 +0530

    Time: 0 ms (100%), Space: 11.3 MB (56.54%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        vector<int> totalSum;
-        int sum = 0;
-        int max = INT_MIN;
+        // vector<int> totalSum;
+        // int sum = 0;
+        int maxWealth = INT_MIN;
         for(int i=0; i<accounts.size(); i++) {
-            vector<int> arr = accounts[i];
-            for(int j=0; j<arr.size(); j++) {
-                sum = sum + arr[j];
+            int sum = 0;
+            for(int j = 0; j<accounts[i].size(); j++) {
+                sum+= accounts[i][j];
             }
-            totalSum.push_back(sum);
-            sum = 0;
-        }
-        for(int k=0; k<totalSum.size(); k++) {
-            if(max <= totalSum[k]) {
-                max = totalSum[k];
-            }
+
+            maxWealth = max(maxWealth, sum); 
         }
 
-        return max;
+        // for(int i=0; i<accounts.size(); i++) {
+        //     vector<int> arr = accounts[i];
+        //     for(int j=0; j<arr.size(); j++) {
+        //         sum = sum + arr[j];
+        //     }
+        //     totalSum.push_back(sum);
+            
+        // }
+        // for(int k=0; k<totalSum.size(); k++) {
+        //     if(max <= totalSum[k]) {
+        //         max = totalSum[k];
+        //     }
+        // }
+
+        return maxWealth;
     }
 };
