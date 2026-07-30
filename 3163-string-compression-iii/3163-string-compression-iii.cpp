e98d19c7d510a154c16552654741c4e1a76ffeb3
Sabasana Aryan <aryan.sabasana.cg@gmail.com>
2026-07-30 14:36:00 +0530

    Time: 27 ms (62.6%), Space: 29.5 MB (47.76%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.cpp
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -5,18 +5,44 @@ public:
         int count = 1;
 
         for(int i=1; i<word.length(); i++) {
-            if(word[i]==word[i-1] && count<9) {
+            if(word[i]==word[i-1]) {
                 count++;
             }
 
             else {
-               
-                res += to_string(count);
-                res += word[i-1];
-                count =1;
+                if(count <= 9) {
+                    res += to_string(count);
+                    res += word[i-1];
+                    count = 1;
+                }
+
+                else {
+                    res += '9' ;
+                    res += word[i-1];
+                    count = count - 9;
+                    while(count > 9) {
+                        res += '9' ;
+                        res += word[i-1];
+                        count=count-9;
+
+                        
+                    }
+                    if(count>=1) {
+                           res += to_string(count);
+                           res += word[i-1];
+                           count = 1;
+                    }
+                }
+                
                 
             }
 
+        }
+
+        while(count > 9) {
+            res += "9";
+            res += word.back();
+            count -= 9;
         }
 
         res += to_string(count);
