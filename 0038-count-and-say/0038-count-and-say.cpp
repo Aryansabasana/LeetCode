68ec511ede65c30567bbfd0c819f5f9336bcc436
Sabasana Aryan <aryan.sabasana.cg@gmail.com>
2026-08-10 13:24:39 +0530

    Time: 4 ms (45.55%), Space: 9.9 MB (63.23%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string ans = "1";
+
+        for (int i = 2; i <= n; i++) {
+            string next = "";
+
+            for (int j = 0; j < ans.length(); ) {
+                int count = 0;
+                char ch = ans[j];
+
+                while (j < ans.length() && ans[j] == ch) {
+                    count++;
+                    j++;
+                }
+
+                next += to_string(count);
+                next += ch;
+            }
+
+            ans = next;
+        }
+
+        return ans;
+    }
+};
