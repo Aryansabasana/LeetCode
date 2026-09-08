be382549e6da6a7fb4114dba9d27c3c67f38023d
Sabasana Aryan <aryan.sabasana.cg@gmail.com>
2026-09-08 10:18:33 +0530

    Time: 15 ms (8.64%), Space: 14.4 MB (10.42%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,20 +9,23 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode *slow = head;
-        ListNode* fast = head;
+       unordered_set<ListNode*> addr;
+       ListNode* i = head;
 
-
-        while(fast!=nullptr && fast->next!=nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-
-
-            if(slow==fast) {
-                return true;
-            }
-        }
-        
+       if(head!=NULL && head->next==NULL) {
         return false;
+       }
+
+       while(i!=NULL) {
+        if(addr.count(i)) {
+            return true;
+        }
+
+        addr.insert(i);
+        i=i->next;
+       }
+
+       return false;
+
     }
 };
